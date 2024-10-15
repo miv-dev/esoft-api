10286aa99beff875b548ee5c9479c2db0b6334f1
Mikhail Ivanov <94083405+miv-dev@users.noreply.github.com>
2024-10-15 17:27:16 +0300

    Some fixes in levenshtein fun

--- src/main/kotlin/com/miv/services/impl/ProfileServiceImpl.kt
+++ src/main/kotlin/com/miv/services/impl/ProfileServiceImpl.kt
@@ -89,16 +89,16 @@ fun levenshtein(lhs: CharSequence?, rhs: CharSequence): Int {
         lhs.isEmpty() -> return rhs.length
         rhs.isEmpty() -> return lhs.length
     }
-    val lhsLength = lhs.length + 1
+    val lhsLength = lhs!!.length + 1
     val rhsLength = rhs.length + 1
 
     var cost = Array(lhsLength) { it }
     var newCost = Array(lhsLength) { 0 }
 
-    for (i in 1..rhsLength - 1) {
+    for (i in 1..<rhsLength) {
         newCost[0] = i
 
-        for (j in 1..lhsLength - 1) {
+        for (j in 1..<lhsLength) {
             val match = if (lhs[j - 1] == rhs[i - 1]) 0 else 1
 
             val costReplace = cost[j - 1] + match
